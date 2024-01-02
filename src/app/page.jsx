1334b6305f90c68a46622d2f5813d7a2b5b9abda
jimitas <nonajun805@gmail.com>
2024-01-02 15:50:06 +0900

    #13

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,9 +9,11 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback((e) => {
-    console.log(count)
+  const handleClick = useCallback(() => {
+    console.log(count);
     if (count < 10) {
       setCount((count) => count + 1);
     }
@@ -24,13 +26,29 @@ export default function Home() {
     };
   }, [count]);
 
+  const handleCange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow(!isShow);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick} className="bg-blue-700 hover:bg-blue-600 text-white rounded px-4 py-2">
         ボタン
       </button>
+      <button onClick={handleDisplay} className="bg-red-700 hover:bg-red-600 text-white rounded px-4 py-2">
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" name="" id="" value={text} onChange={handleCange} />
       <Main page="index" />
       <Footer />
     </main>
